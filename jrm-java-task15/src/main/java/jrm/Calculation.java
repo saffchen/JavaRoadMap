a901f91074de49d1b7b0d73b21556df29cb1c432
Денис <denis.savchenko98@yandex.ru>
2022-04-06 20:26:38 +0300

    Task 11

---
+++ jrm-java-task15/src/main/java/jrm/Calculation.java
@@ -0,0 +1,12 @@
+package jrm;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a != 11){
+            System.out.println("отсчет" + a);
+            ++a;
+        }
+        System.out.println("расчет закончен");
+    }
+}
