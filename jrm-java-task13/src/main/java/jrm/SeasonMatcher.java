977b3ce64f84443b02ead27ddf19260c1ac1ecdb
Денис <denis.savchenko98@yandex.ru>
2022-04-06 18:23:18 +0300

    Task 9

--- jrm-java-task13/src/main/java/jrm/SeasonMatcher.java
+++ jrm-java-task13/src/main/java/jrm/SeasonMatcher.java
@@ -8,18 +8,17 @@ public class SeasonMatcher {
     String autumn = "переданный месяц означает, что на улице осень";
     String invalid = "переданное число невалидно";
 
+
     public void match(int month) {
-        if(month == 12 || month == 1 || month == 2){
+        if (month == 12 || month == 1 || month == 2) {
             System.out.println(winter);
-        }
-        else if(month == 3 || month == 4 || month == 5){
+        } else if (month == 3 || month == 4 || month == 5) {
             System.out.println(spring);
-        }
-        else if(month == 6 || month == 7 || month == 8){
+        } else if (month == 6 || month == 7 || month == 8) {
             System.out.println(summer);
-        }
-        else if(month == 9 || month == 10 || month == 11){
+        } else if (month == 9 || month == 10 || month == 11) {
             System.out.println(autumn);
         } else System.out.println(invalid);
     }
 }
+
--- jrm-java-task13/src/main/java/jrm/ShowResult.java
+++ jrm-java-task13/src/main/java/jrm/ShowResult.java
@@ -3,6 +3,7 @@ package jrm;
 public class ShowResult {
 
     public static void main(String[] args) {
+
      new SeasonMatcher().match(1);
     }
 }
