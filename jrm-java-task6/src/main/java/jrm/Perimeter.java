602c98f82dd4be4f2f9e00636fa90daf107d2798
Денис <denis.savchenko98@yandex.ru>
2022-04-06 16:43:02 +0300

    Task 4

---
+++ jrm-java-task6/src/main/java/jrm/Perimeter.java
@@ -0,0 +1,12 @@
+package jrm;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        int radius = 10;
+        double p = 3.1415;
+        double perimeter;
+
+        perimeter = 2 * radius * p;
+        System.out.println(perimeter);
+    }
+}
