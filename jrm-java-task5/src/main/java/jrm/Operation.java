0c851c060d060be1d75d988d2d0846f60a20b890
Денис <denis.savchenko98@yandex.ru>
2022-04-06 16:42:41 +0300

    Task 3

---
+++ jrm-java-task5/src/main/java/jrm/Operation.java
@@ -0,0 +1,13 @@
+package jrm;
+
+public class Operation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.println(a % b);
+        System.out.println(a - b);
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
