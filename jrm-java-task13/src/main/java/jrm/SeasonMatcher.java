12a1a8ea7d885765eceb2e7618dd957c7223cf81
Денис <denis.savchenko98@yandex.ru>
2022-04-06 18:16:26 +0300

    Task 9

---
+++ jrm-java-task13/src/main/java/jrm/SeasonMatcher.java
@@ -0,0 +1,25 @@
+package jrm;
+
+public class SeasonMatcher {
+
+    String winter = "переданный месяц означает, что на улице зима";
+    String spring = "переданный месяц означает, что на улице весна";
+    String summer = "переданный месяц означает, что на улице лето";
+    String autumn = "переданный месяц означает, что на улице осень";
+    String invalid = "переданное число невалидно";
+
+    public void match(int month) {
+        if(month == 12 || month == 1 || month == 2){
+            System.out.println(winter);
+        }
+        else if(month == 3 || month == 4 || month == 5){
+            System.out.println(spring);
+        }
+        else if(month == 6 || month == 7 || month == 8){
+            System.out.println(summer);
+        }
+        else if(month == 9 || month == 10 || month == 11){
+            System.out.println(autumn);
+        } else System.out.println(invalid);
+    }
+}
