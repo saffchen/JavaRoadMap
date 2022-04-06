431fbdc07f7dab2ba6c39a2bc7dddb9695e938eb
Денис <denis.savchenko98@yandex.ru>
2022-04-06 18:15:27 +0300

    Task 8

---
+++ jrm-java-task12/src/main/java/jrm/MethodActions.java
@@ -0,0 +1,11 @@
+package jrm;
+
+public class MethodActions {
+    void print(String text){
+        System.out.println(text);
+    }
+    void invokeMethod(){
+        String name = "Petr";
+        print(name);
+    }
+}
---
+++ jrm-java-task12/src/main/java/jrm/ShowResults.java
@@ -0,0 +1,8 @@
+package jrm;
+
+public class ShowResults {
+    public static void main(String[] args) {
+        MethodActions methodActions = new MethodActions();
+            methodActions.invokeMethod();
+    }
+}
