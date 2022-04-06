52acbe00790533d825389adff0150b8bae38b88d
Денис <denis.savchenko98@yandex.ru>
2022-04-06 16:43:27 +0300

    Task 5

---
+++ jrm-java-task8/src/main/java/jrm/Operations.java
@@ -0,0 +1,10 @@
+package jrm;
+
+public class Operations {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        System.out.println(a++);
+        System.out.println(++b);
+    }
+}
