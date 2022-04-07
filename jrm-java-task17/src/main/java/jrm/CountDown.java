5cbad9a27b31ed8066994f8f48b714bca815d4ae
Денис <denis.savchenko98@yandex.ru>
2022-04-07 15:51:27 +0300

    Task 13

---
+++ jrm-java-task17/src/main/java/jrm/CountDown.java
@@ -0,0 +1,9 @@
+package jrm;
+
+public class CountDown {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i-- ){
+            System.out.println(i);
+        }
+    }
+}
