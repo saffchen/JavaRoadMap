7500afe60e804c4b7994ca454b61a43b30076158
Денис <denis.savchenko98@yandex.ru>
2022-04-14 21:47:58 +0300

    Task 30 Вывод IllegalArgumentException

---
+++ jrm-java-task52/src/main/java/jrm/App.java
@@ -0,0 +1,14 @@
+package jrm;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args){
+        User user = new User();
+        user.setEmail("email");
+        user.setFirstName("name");
+        user.setPhone("11");
+        UserDataValidator.validatePhoneNumber(user);
+    }
+}
