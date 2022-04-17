372315c2b7c43deb906ecc12843f5cff0a61c9b2
Денис <denis.savchenko98@yandex.ru>
2022-04-17 19:18:07 +0300

    Task 32

--- jrm-java-task55/src/main/java/jrm/ReadTxt.java
+++ jrm-java-task55/src/main/java/jrm/ReadTxt.java
@@ -7,21 +7,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class ReadTxt {
-    public static void main(String[] args) {
-        byte[] rawString = null;
+    public static void main(String args[]) {
         String fileName = "readMe.txt";
-        ClassLoader classLoader = ReadTxt.class.getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
-        try {
-            rawString = Files.readAllBytes(file.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] allRaws = new String(rawString, StandardCharsets.UTF_8).split("\n");
-        for (String row : allRaws){
-            if (row.contains("Apple")){
-                System.out.println(row);
-            }
-        }
+
     }
 }
